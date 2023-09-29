2130201148e44e3f6ce2c0bb238b21d6f1309e31
Mykhailo Bodnar <mykhailo.bodnar@wetelo.com>
2023-09-29 07:47:35 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// JavaScript Document
+
+$(document).ready(function() {
+	var showChar = 100;
+	var ellipsestext = "...";
+	var moretext = "more";
+	var lesstext = "less";
+	$('.more').each(function() {
+		var content = $(this).html();
+
+		if(content.length > showChar) {
+
+			var c = content.substr(0, showChar);
+			var h = content.substr(showChar-1, content.length - showChar);
+
+			var html = c + '<span class="moreelipses">'+ellipsestext+'</span>&nbsp;<span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">'+moretext+'</a></span>';
+
+			$(this).html(html);
+		}
+
+	});
+
+	$(".morelink").click(function(){
+		if($(this).hasClass("less")) {
+			$(this).removeClass("less");
+			$(this).html(moretext);
+		} else {
+			$(this).addClass("less");
+			$(this).html(lesstext);
+		}
+		$(this).parent().prev().toggle();
+		$(this).prev().toggle();
+		return false;
+	});
+	$(".content-faq").hide();
+	$(".show_hide").click(function(){
+        var txt = $(".content-faq").is(':visible') ? 'See All' : 'Less All';
+        $(".show_hide").text(txt);
+        $(this).prev('.content-faq').slideToggle(200);
+    });
+});
